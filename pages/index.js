0c45e0c9da62ddccbbc4020afbd5b6109f6d60d4
vasili cekaskin <vasili.cekaskin@invision.de>
2021-03-18 19:12:09 +0100

    Remove beta artifacts

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,6 @@ export default function Home() {
         <title>Colorool</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1 style={{ textAlign: "center", color: "white" }}>Colorool</h1>
-      <h1 style={{ textAlign: "center", color: "white" }}>
-        Something nice is coming soon! Just come back later :)
-      </h1>
     </>
   );
 }
