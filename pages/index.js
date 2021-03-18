ee47b5c97e4dd14ff13d3544bac5eea178ceaf13
vasili cekaskin <vasili.cekaskin@invision.de>
2021-03-18 18:42:08 +0100

    Setup placeholer home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-      <div></div>
-  )
+    <>
+      <h1 style={{ textAlign: "center", color: "white" }}>Colorool</h1>
+      <h1 style={{ textAlign: "center", color: "white" }}>
+        Something nice is coming soon! Just come back later :)
+      </h1>
+    </>
+  );
 }
