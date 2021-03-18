75dbcf94195ab6765e1559a266a8554a9194911c
vasili cekaskin <vasili.cekaskin@invision.de>
2021-03-18 19:01:06 +0100

    Setup demo favicon and title

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Colorool</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1 style={{ textAlign: "center", color: "white" }}>Colorool</h1>
       <h1 style={{ textAlign: "center", color: "white" }}>
         Something nice is coming soon! Just come back later :)
