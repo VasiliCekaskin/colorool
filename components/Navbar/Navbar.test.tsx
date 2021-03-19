6559463209422ad5960dfb9d420fcf9e603a258e
Vasili Cekaskin <vasili.cekaskin@invision.de>
2021-03-19 19:25:15 +0100

    Write first two tests for navbar

--- components/Navbar/Navbar.test.tsx
+++ components/Navbar/Navbar.test.tsx
@@ -1,10 +1,24 @@
 import Navbar from "./Navbar";
-import { findByDisplayValue, render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 
 describe("Navbar", () => {
-  it("has a Home link", () => {
+  it("has all required links", () => {
     const { getByText } = render(<Navbar />);
 
-    expect(getByText("Home")).toBeInTheDocument();
+    const homeLink = getByText("Home");
+    const colorPalettes = getByText("Color Palettes");
+    const helpCenter = getByText("Help Center");
+
+    expect(homeLink).toBeInTheDocument();
+    expect(colorPalettes).toBeInTheDocument();
+    expect(helpCenter).toBeInTheDocument();
+  });
+
+  it("has a sign in link/button", () => {
+    const { getByText } = render(<Navbar />);
+
+    const signInLink = getByText("Sign In");
+
+    expect(signInLink).toBeInTheDocument();
   });
 });
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <>
-      <div>Home</div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/colorpalettes">
+        <a>Color Palettes</a>
+      </Link>
+      <Link href="/help">
+        <a>Help Center</a>
+      </Link>
+      <Link href="/signin">
+        <a>Sign In</a>
+      </Link>
     </>
   );
 };
