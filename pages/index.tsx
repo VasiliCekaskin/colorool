17cc8a65ced4e72800ff312d27088635b4ebefc9
Vasili Cekaskin <vasili.cekaskin@invision.de>
2021-03-19 17:28:29 +0100

    Use nice syntax for Index Home module

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar/Navbar";
+import styles from "../styles/Index.module.css";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
         <title>Colorool</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
+        <Navbar />
     </>
   );
 }
+
+export default Home;
